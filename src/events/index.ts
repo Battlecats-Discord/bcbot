8e6d3c103be8d8798558382d5bf8e85005343af9
Googlefan256 <yyykakeru@gmail.com>
2023-02-09 22:44:55 +0900

    fix: 🐛 no file

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,6 +1,5 @@
 import { ReadyEvent } from "./ready";
 import { InteractionEvent } from "./interaction";
 import { GuildMemberAddEvent } from "./guildmemberadd";
-import { VoiceStateUpdateEvent } from "./voiceState";
 
-export default [ReadyEvent, InteractionEvent, GuildMemberAddEvent,VoiceStateUpdateEvent];
+export default [ReadyEvent, InteractionEvent, GuildMemberAddEvent];
