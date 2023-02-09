9eefcc5a8d71610b3fe9f217744d1a3b43fff9ab
Googlefan256 <yyykakeru@gmail.com>
2023-02-09 23:18:54 +0900

    fix: 🐛 bug

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -9,13 +9,13 @@ export const ReadyEvent: Event<"ready"> = {
 			commands.push(command.data.toJSON());
 		}
 		client.application?.commands.set(commands);
-		setInterval(() => {
-			console.log(getEmpty(client))
+		setInterval(async () => {
+			console.log(await getEmpty(client))
 		}, 1000 * 60 * 5);
-		console.log(getEmpty(client))
-		process.stdin.on("data", d => {
+		console.log(await getEmpty(client))
+		process.stdin.on("data", async d => {
 			if (d.toString().trim() === "get") {
-				console.log(getEmpty(client));
+				console.log(await getEmpty(client));
 			}
 		})
 	},
--- src/lib/voice.ts
+++ src/lib/voice.ts
@@ -1,28 +1,30 @@
-import { ChannelType } from "discord.js";
+import { Channel, ChannelType, VoiceChannel } from "discord.js";
 import { Client } from "../structures/client";
 
 const parent = process.env.VC_PARENT;
 const excluded = process.env.VC_EXCLUDED?.split(",") ?? [];
 const guild = process.env.GUILD_ID;
 
-export function getEmpty(client: Client) {
+export async function getEmpty(client: Client) {
 	if (!(excluded.length && parent && guild)) return [];
 	const vcs = client.channels.cache.filter(
-		(c) => "parentId" in c && c.parentId === parent
+		(c) =>
+			"parentId" in c &&
+			c.parentId === parent &&
+			c.type === ChannelType.GuildVoice &&
+			c.guildId === guild &&
+			!excluded.includes(c.id) &&
+			c.createdTimestamp + 1000 * 60 * 5 < Date.now()
 	);
-	return vcs
-		.filter(
-			(x) =>
-				!excluded.includes(x.id) &&
-				x.type === ChannelType.GuildVoice &&
-				x.guildId === guild &&
-				x.createdTimestamp + 1000 * 60 * 5 < Date.now() && x.members.size === 0
-		)
-		.map((x) => x.id);
+	const vcList: VoiceChannel[] = [];
+	for (const vc of vcs.values()) {
+		vcList.push((await vc.fetch()) as VoiceChannel);
+	}
+	return vcList.filter((x) => x.members.size === 0).map((x) => x.id);
 }
 
-export function purgeEmpty(client: Client) {
-	const empty = getEmpty(client);
+export async function purgeEmpty(client: Client) {
+	const empty = await getEmpty(client);
 	for (const vc of empty) {
 		client.channels.cache.get(vc)?.delete();
 	}
